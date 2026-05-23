9f6267eeef82c35980159664451c0fb9875ab9c0
안강현 <gimon0331@gmail.com>
2026-05-23 18:27:57 +0900

    Expose Riot API utilities from utils package

---
+++ utils/riot.py
@@ -0,0 +1,9 @@
+from aira.riot import LeagueEntry, MatchSummary, RiotAccount, RiotApiError, RiotClient
+
+__all__ = [
+    "LeagueEntry",
+    "MatchSummary",
+    "RiotAccount",
+    "RiotApiError",
+    "RiotClient",
+]
