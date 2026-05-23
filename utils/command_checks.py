6ecc85d451b3cb061cf502820bce2f727c98c5ec
안강현 <gimon0331@gmail.com>
2026-05-23 18:27:32 +0900

    Expose command guard utilities

---
+++ utils/command_checks.py
@@ -0,0 +1,8 @@
+from aira.checks import RegisterView, ensure_registered, registered, require_role
+
+__all__ = [
+    "RegisterView",
+    "ensure_registered",
+    "registered",
+    "require_role",
+]
