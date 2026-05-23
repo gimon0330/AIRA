3ab67d4144cec989899709b948e64160b91e94b5
안강현 <gimon0331@gmail.com>
2026-05-23 11:04:16 +0900

    Add package executable entrypoint

---
+++ aira/__main__.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+import logging
+
+from aira.bot import AiraBot
+from aira.config import load_settings
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
+
+settings = load_settings()
+AiraBot(settings).run(settings.discord_token)
